ffb0d237460d07649a7e8f6be791d5a54bea424c
Eyal Azran <azran4u@gmail.com>
2020-12-06 13:15:33 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  styleUrls: ['./app.component.less'],
 })
 export class AppComponent {
   title = 'angular-material-rxjs';
+
+  openedChange(event: boolean) {
+    console.log(`sidenav openedChange changed value=${event}`);
+  }
+
+  closedStart(event: void) {
+    console.log(`sidenav closedStart changed value=${event}`);
+  }
 }

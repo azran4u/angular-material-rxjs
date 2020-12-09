e0db48148edf341a01f836ab06765deb3d6a48ad
Eyal Azran <azran4u@gmail.com>
2020-12-09 14:07:13 +0200

    added defualt route

--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-import { GithubSmartComponent } from '../github/smart/github-smart/github-smart.component';
-import { HomeComponent } from '../home/home/home.component';
 import { NavigationSmartComponent } from '../navigation/smart/navigation-smart.component';
 
 const routes: Routes = [
   {
     path: '',
+    pathMatch: 'full',
     component: NavigationSmartComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
   },
 ];
 
